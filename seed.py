17dcc7c7a5356104dee571d315ea8ae0560d16e8
U-Lenovo-PC\rmurphy <bekkam@users.noreply.github.com>
2016-03-02 08:36:14 -0800

    Write function to seed database with outage data

--- seed.py
+++ seed.py
@@ -1,9 +1,11 @@
 """Utility file to seed streetlight outage database with SF OpenData SF311 cases"""
 
+from model import Outage, connect_to_db, db
+from server import app
 import re
 
 
-def get_outage_latlngs():
+def get_outages():
     """Get streetlight outage lat longs from data file."""
 
     data_file = open("streetlight-data.csv")
@@ -16,14 +18,21 @@ def get_outage_latlngs():
             # print latlng
             # print type(latlng)
             pairs = latlng.split(',')
-            print "pairs is ", pairs
-            lat = pairs[0]
-            lng = pairs[1]
-            # print "lat:", lat
-            # print "lng:", lng
+            # print "pairs is ", pairs
+            outage_lat = pairs[0]
+            outage_long = pairs[1].lstrip()
+            # print outage_long
 
-            # TODO: add each pair to db, after confirming format is appropriate for google markers
-    #         streetlight = Streetlight(lat=lat, lng=lng)
-    #         db.session.add(streetlight)
+            outage = Outage(outage_lat=outage_lat, outage_long=outage_long)
+            db.session.add(outage)
 
-    # db.session.commit()
+    print "Finished seeding database"
+    db.session.commit()
+
+
+# #####################################3
+if __name__ == '__main__':
+    connect_to_db(app)
+    db.create_all()
+
+    get_outages()
