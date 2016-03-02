a5f01e11ee62080a5d2fdcd0cd8de9ddcc777f2a
U-Lenovo-PC\rmurphy <bekkam@users.noreply.github.com>
2016-03-01 22:28:58 -0800

    Get streetlight outage latlngs from (test) data file

--- seed.py
+++ seed.py
@@ -3,34 +3,21 @@ import re
 """Utility file to seed streetlight outage database with SF OpenData SF311 cases"""
 
 
-def d():
-    """Load streetlight outage lat longs into database."""
+def get_outage_latlngs():
+    """Get streetlight outage lat longs from data file."""
 
     data_file = open("test-data.txt")
     for row in data_file:
-        # print line.split(',')[-1]
         row = row.rstrip()
-        # latlng = row.split(',')[14:15]
-        # print latlng
 
-        # print re.search(r"org", row)
-
-        # prints org :)
-        # point = re.search(r"org", row)
-        # if point:
-        #     print point.group(0)
-
-        point = re.search(r"\((\d)*\.(\d)*\,\s(\d*)", row)
-        # print point
+        point = re.search(r"(\d)*\.(\d)*\,\s\-(\d)*\.(\d)*", row)
         if point:
-            print point.group(0)
-            print point.group(1)
-        # print "line is", row
-        # items = line.split(',').rstrip()
-        # print "items in line are", items
-
-
-        # print items[-2]
-        # print item[0]
-        # for i in item:
-        #     print i
+            latlng = point.group(0)
+            print latlng
+            print type(latlng)
+            pairs = latlng.split(',')
+            print pairs
+            lat = pairs[0]
+            lng = pairs[1]
+            print "lat:", lat
+            print "lng:", lng
