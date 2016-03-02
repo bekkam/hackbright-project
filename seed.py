a2dd735e7c7d4a12347a89dd85c5106dd9434848
U-Lenovo-PC\rmurphy <bekkam@users.noreply.github.com>
2016-03-01 22:45:01 -0800

    Comment out many of the debugging statements

--- seed.py
+++ seed.py
@@ -6,18 +6,20 @@ import re
 def get_outage_latlngs():
     """Get streetlight outage lat longs from data file."""
 
-    data_file = open("test-data.txt")
+    data_file = open("test-data.csv")
     for row in data_file:
         row = row.rstrip()
 
         point = re.search(r"(\d)*\.(\d)*\,\s\-(\d)*\.(\d)*", row)
         if point:
             latlng = point.group(0)
-            print latlng
-            print type(latlng)
+            # print latlng
+            # print type(latlng)
             pairs = latlng.split(',')
-            print pairs
-            lat = pairs[0]
-            lng = pairs[1]
-            print "lat:", lat
-            print "lng:", lng
+            print "pairs is ", pairs
+            # lat = pairs[0]
+            # lng = pairs[1]
+            # print "lat:", lat
+            # print "lng:", lng
+
+            # TODO: add each pair to db, after confirming format is appropriate for google markers
