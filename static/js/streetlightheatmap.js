3455ea6165b745d5b2406f43e044ff8959694f4e
U-Lenovo-PC\rmurphy <bekkam@users.noreply.github.com>
2016-03-17 17:22:19 -0700

    Toggle heatmap on user click

--- static/js/streetlightheatmap.js
+++ static/js/streetlightheatmap.js
@@ -1,5 +1,5 @@
 var latLngs = [];
-
+var heatmap;
 function generateHeatMap(evt) {
   alert("generateHeatMap called");
   // Make the query string for SODA API
@@ -9,16 +9,23 @@ function generateHeatMap(evt) {
 
     // Get data, and lat/long to latLngs array
     $.getJSON(url, function(data) {
-      console.log(data);
           $.each(data, function(i, entry) {
               latLngs.push(new google.maps.LatLng(parseFloat(entry.point.latitude), 
                                                    parseFloat(entry.point.longitude)));
             });
     });
-    var heatmap = new google.maps.visualization.HeatmapLayer({
+    heatmap = new google.maps.visualization.HeatmapLayer({
         data: latLngs,
         map: map
     }); 
 }
 
-$("#heatmap").on("change", generateHeatMap);
+function toggleHeatMap(evt){
+  if ( $( "#heatmap" ).prop( "checked" ) ) {
+    generateHeatMap();
+  } else {
+    heatmap.setMap(null);
+  }
+}
+
+$("#heatmap").on("change", toggleHeatMap);
