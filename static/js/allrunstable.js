f942c75469820d082ed9d47d104e03c4f7fb30f5
U-Lenovo-PC\rmurphy <bekkam@users.noreply.github.com>
2016-03-24 21:21:42 -0700

    Use jQuery to create run list table when DOM is fully loaded

--- static/js/allrunstable.js
+++ static/js/allrunstable.js
@@ -1,15 +1,20 @@
 "use strict";
 
-$.getJSON('all-run-data.json', function(data) {
-      $.each(data, function(runId, ranRoute) {
-            console.log(runId);
-            console.log(ranRoute);
-            var row = $("<tr />")
-            $("#allRunsDataTable").append(row); 
-            row.append($("<td>" + "<a href=" + "/runs/" + runId + ">"+ runId + "</td>"));
-            row.append($("<td>" + ranRoute.route_name + "</td>"));
-            row.append($("<td>" + ranRoute.run_date + "</td>"));
-            row.append($("<td>" + ranRoute.route_distance + "</td>"));
-            row.append($("<td>" + ranRoute.duration + "</td>"));
-        });
+
+$( document ).ready(function() {
+      $('#allRunsDataTable').html("<tr><th>Run ID</th><th>Route Name</th><th>Date of Run</th><th>Distance (km)</th><th>Duration</th></tr>")
+      
+      $.getJSON('all-run-data.json', function(data) {
+            $.each(data, function(runId, ranRoute) {
+                  console.log(runId);
+                  console.log(ranRoute);
+                  var row = $("<tr />")
+                  $("#allRunsDataTable").append(row); 
+                  row.append($("<td>" + "<a href=" + "/runs/" + runId + ">"+ runId + "</td>"));
+                  row.append($("<td>" + ranRoute.route_name + "</td>"));
+                  row.append($("<td>" + ranRoute.run_date + "</td>"));
+                  row.append($("<td>" + ranRoute.route_distance + "</td>"));
+                  row.append($("<td>" + ranRoute.duration + "</td>"));
+              });
+      });
 });
