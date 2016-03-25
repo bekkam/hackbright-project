0fb4b50a43994837f71dacd1bf64361eab0776cf
U-Lenovo-PC\rmurphy <bekkam@users.noreply.github.com>
2016-03-24 21:30:43 -0700

    Use jQuery to create route list table when DOM is fully loaded

--- static/js/allroutestable.js
+++ static/js/allroutestable.js
@@ -1,15 +1,18 @@
 "use strict";
 
-$.getJSON('all-route-data.json', function(data) {
-      $.each(data, function(id, route) {
-            console.log(id);
-            console.log(route);
-            var row = $("<tr />")
-            $("#allRoutesDataTable").append(row); 
-            row.append($("<td>" + id + "</td>"));
-            row.append($("<td>" + "<a href=" + "/routes/" + id + ">" + route.route_name + "</td>"));
-            row.append($("<td>" + route.add_date + "</td>"));
-            row.append($("<td>" + route.route_distance + "</td>"));
+$( document ).ready(function() {
+      $('#allRoutesDataTable').html("<tr><th>Route ID</th><th>Route Name</th><th>Date Added</th><th>Distance (km)</th></tr>")
 
-        });
+      $.getJSON('all-route-data.json', function(data) {
+            $.each(data, function(id, route) {
+                  var row = $("<tr />")
+                  $("#allRoutesDataTable").append(row); 
+                  row.append($("<td>" + id + "</td>"));
+                  row.append($("<td>" + "<a href=" + "/routes/" + id + ">" + route.route_name + "</td>"));
+                  row.append($("<td>" + route.add_date + "</td>"));
+                  row.append($("<td>" + route.route_distance + "</td>"));
+
+              });
+      });
+
 });
