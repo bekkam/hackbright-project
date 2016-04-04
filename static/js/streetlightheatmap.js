5054643be2f3be310f85f206d941fd214cf5bb42
U-Lenovo-PC\rmurphy <bekkam@users.noreply.github.com>
2016-04-04 15:16:07 -0700

    Tidy formatting

--- static/js/streetlightheatmap.js
+++ static/js/streetlightheatmap.js
@@ -4,31 +4,30 @@ var heatmap;
 var latLngs = [];
 
 function generateHeatMap(evt) {
-  // Make the query string for SODA API
-  url = "https://data.sfgov.org/resource/vw6y-z8j6.json?"
-    +"category=Streetlights"
-    +"&Status=Open"
-
+    // Make the query string for SODA API
+    url = "https://data.sfgov.org/resource/vw6y-z8j6.json?"
+          +"category=Streetlights"
+          +"&Status=Open"
 
     // Get data, and lat/long to latLngs array
     $.getJSON(url, function(data) {
-          $.each(data, function(i, entry) {
-              latLngs.push(new google.maps.LatLng(parseFloat(entry.point.latitude), 
-                                                  parseFloat(entry.point.longitude)));
-            });
+        $.each(data, function(i, entry) {
+            latLngs.push(new google.maps.LatLng(parseFloat(entry.point.latitude), 
+                                                parseFloat(entry.point.longitude)));
+        });
+        heatmap = new google.maps.visualization.HeatmapLayer({
+            data: latLngs,
+            map: map
+        });
     });
-    heatmap = new google.maps.visualization.HeatmapLayer({
-        data: latLngs,
-        map: map
-    }); 
 }
 
 function toggleHeatMap(evt){
-  if ( $( "#heatmap" ).prop( "checked" ) ) {
-    generateHeatMap();
-  } else {
-    heatmap.setMap(null);
-  }
+    if ($("#heatmap").prop("checked")) {
+        generateHeatMap();
+    } else {
+        heatmap.setMap(null);
+    }
 }
 
 $("#heatmap").on("change", toggleHeatMap);
